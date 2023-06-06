8936dc4925c9ce96f50437a15658d81dab6d7de7
DwiKrisnandi1905 <dwi.krisnandi05@gmail.com>
2023-06-06 20:39:37 +0700

    first commit

---
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+@section('title', 'Halaman Dashboard')
+
+@section('header', 'Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Petugas</div>
+                <div class="card-body">
+                    <div class="text-center">
+                        {{ $petugas }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Masyarakat</div>
+                <div class="card-body">
+                <div class="text-center">
+                        {{ $masyarakat }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Pengaduan Proses</div>
+                <div class="card-body">
+                <div class="text-center">
+                        {{ $proses }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Pengaduan Selesai</div>
+                <div class="card-body">
+                <div class="text-center">   
+                        {{ $selesai }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-center" style="width: 100%">
+            <a href="{{ route('pekat.index') }}" class="btn btn-warning text-white mt-3">Kembali ke Halaman Utama</a>
+        </div>
+    </div>
+@endsection
